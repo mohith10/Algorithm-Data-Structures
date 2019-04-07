47d7d67e1b3bfbe0aa47e4bf721e679be9bc6ab3
Mohit Hooda <mhooda87@gmail.com>
2019-04-07 11:44:43 +0530

    Create ArrayRotation_Reversal_Algo.cpp

---
+++ ArrayRotation_Reversal_Algo.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void fast(){ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);}
+ll a[100];
+void reverse(ll start,ll end)
+{
+    while(start<end)
+        swap(a[start++],a[end--]);
+
+}
+void printarray(ll n)
+{
+    for(ll i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+}
+int main()
+{
+    ll n;
+    cin>>n;
+
+    for(ll i=0;i<n;i++)
+        cin>>a[i];
+    ll d;
+    cin>>d;
+    // Reversal algorithm
+    ll l=0,r=(n-(d%n));
+    reverse(0,r-1);
+    reverse(r,n-1);
+    reverse(0,n-1);
+    printarray(n);
+
+
+
+}
