5d959689feb161a83b4cbf2b2b33e356bb37ed03
Mohit Hooda <mhooda87@gmail.com>
2019-04-26 12:43:23 +0530

    Create GFG_Linked_List_Reverse_List.cpp

---
+++ GFG_Linked_List_Reverse_List.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+struct node{
+    ll data;
+    struct node *next;
+};
+
+struct node* insert_head(struct node* head, ll value)
+{
+    struct node* new_node=(struct node*)(malloc(sizeof(struct node)));
+    new_node->data=value;
+    new_node->next=head;
+    head=new_node;
+    return head;
+    
+}
+struct node* insert_tail(struct node* head,ll value)
+{
+    struct node* new_node=(struct node*)(malloc(sizeof(struct node)));
+    new_node->data=value;
+    new_node->next=NULL;
+    struct node* temp_point=head;
+    while(temp_point->next!=NULL)
+        temp_point=temp_point->next;
+    temp_point->next=new_node;
+    return head;
+    
+}
+void display(struct node* head)
+{
+    struct node* temp_head=head;
+    cout<<"Linked List is "<<endl;
+    while(temp_head!=NULL)
+    {
+        cout<<temp_head->data<<" ";
+        temp_head=temp_head->next;
+    }
+    cout<<endl;
+}
+struct node* reverse(struct node* head)
+{
+    struct node* prev=NULL;
+    struct node* curr=head;
+    struct node* next_node;
+    while(curr!=NULL)
+    {
+        next_node=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next_node;
+    }
+    head=prev;
+    return prev;
+}
+int main() {
+    
+    struct node *head=NULL;
+    head=insert_head(head,2);
+    //cout<<"Value inserted "<<head->data<<endl;
+    head=insert_head(head,3);
+    head=insert_head(head,4);
+    head=insert_tail(head,1);
+    display(head);
+    head=reverse(head);
+    display(head);
+}
