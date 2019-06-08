7e1301f92d2cc495131c79e9d8ebf99fceb47bf0
Mohit Hooda <mhooda87@gmail.com>
2019-06-08 23:00:22 +0530

    Create BST_Dead_End_Check.cpp

---
+++ Binary Search Tree/BST_Dead_End_Check.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+struct node{
+    int data;
+    node *left,*right;
+};
+
+node* insert(struct node* root,int value)
+{
+   
+    if(root==NULL)
+    {
+        struct node* point=(struct node*)(malloc(sizeof(struct node)));
+        point->data=value;
+        point->left=NULL;
+        point->right=NULL;
+        root=point;
+        return root;
+    }
+    if(value<root->data)
+    {
+        root->left=insert(root->left,value);
+    }
+    else
+    {
+        root->right=insert(root->right,value);
+    }
+    return root;
+}
+void preorder(struct node* root)
+{
+    if(root==NULL)
+        return ;
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+    
+}
+bool Dead_End_Check(struct node* root,int min,int max)
+{
+    if(root==NULL)
+        return false;
+    if(min==max)
+        return true;
+    return ((Dead_End_Check(root->left,min,root->data-1))||(Dead_End_Check(root->right,root->data+1,max)));
+}
+int main() {
+	struct node *root=NULL;
+	root=insert(root,50);
+	insert(root, 30); 
+    insert(root, 20); 
+    insert(root, 40); 
+    insert(root, 70); 
+    insert(root, 60); 
+    insert(root, 80); 
+    cout<<"The tree is (Preorder): "<<endl;
+    preorder(root);
+    cout<<endl;
+    
+    if(Dead_End_Check(root,1,INT_MAX))
+        cout<<"Dead End present";
+    else cout<<"No Dead-END";
+}
