94ca506cd7b0a91496034bab37fbb1922a7708bd
Mohit Hooda <mhooda87@gmail.com>
2022-04-09 23:15:15 +0530

    Create stockbuy or sell.cpp

---
+++ stockbuy or sell.cpp
@@ -0,0 +1,21 @@
+
+
+#include <std/c++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int max_profit = 0, min_price = 1000;
+    for(int i=0;i<n;i++){
+        min_price = min(a[i],min_price);
+        max_profit = max(max_profit,a[i]-min-price)
+    }
+    cout<<max_price;
+}
